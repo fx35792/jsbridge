0ba1b64ed1ae6a4e3753a2d6c2a0373cc3873851
sunnyFan <fx35792@163.com>
2020-01-10 17:48:21 +0800

    JSBridge 实现方式一：拦截 URL Schema

--- native/app/src/main/java/com/example/jsbridge/MainActivity.java
+++ native/app/src/main/java/com/example/jsbridge/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.jsbridge;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private EditText editText;
+    private Button showBtn;
     private Button refreshBtn;
+    private MainActivity self = this;
 
     Date date = new Date();
 
@@ -22,9 +29,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webView);
+        editText = findViewById(R.id.editText);
+        showBtn = findViewById(R.id.showBtn);
         refreshBtn = findViewById(R.id.refreshBtn);
 
         webView.loadUrl("http://192.168.3.36:8080/?timeStamp=" + date.getTime());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                if (!message.startsWith("jsbridge://")) {
+                    return super.onJsAlert(view, url, message, result);
+                }
+
+                String text = message.substring(message.indexOf("=") + 1);
+                self.showNativeDialog(text);
+                result.confirm();
+                return true;
+            }
+        });
+
+        showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputValue = editText.getText().toString();
+
+                self.showWebDialog(inputValue);
+            }
+        });
 
         refreshBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +64,15 @@ public class MainActivity extends AppCompatActivity {
                 webView.loadUrl("http://192.168.3.36:8080/?timeStamp=" + date.getTime());
             }
         });
+    }
+
+    private void showWebDialog(String text) {
+        String jsCode = String.format("window.showWebDialog('%s')", text);
+        webView.evaluateJavascript(jsCode, null);
+    }
+
+    private void showNativeDialog(String text) {
+        //调取原生方法
+        new AlertDialog.Builder(this).setMessage(text).create().show();
     }
 }
